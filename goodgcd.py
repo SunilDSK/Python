0ad927530688cf25d3f5cadf6d92d581f2fb601a
Sunilkumar <sunillangtad54@gmail.com>
2017-07-11 23:19:10 +0530

    Eliminate the list and keep the most recent common factor

--- goodgcd.py
+++ goodgcd.py
@@ -3,9 +3,9 @@ def gcd(n,m):
     cf = []
     for i in range(1,min(n,m)+1):
         if ((m%i) == 0) and ((n%i) == 0):
-            cf.append(i)
+            mrcf=i
     
-    print(cf[-1])
+    print(mrcf)
 
 print("This program will print GCD of two numbers")
 gcd(16,8)
