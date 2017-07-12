8e27b405d094c9fd804395bcba8d2bf785e048f1
Sunilkumar <sunillangtad54@gmail.com>
2017-07-12 09:42:49 +0530

    Small and concise gcd

--- gcd.py
+++ gcd.py
@@ -1,21 +1,11 @@
-print("This is a GCD program")
-
-def gcd(m,n):
-    fm = []
-    for i in range(1,m+1):
-        if (m%i) == 0:
-            fm.append(i)
-
-    fn = []
-    for i in range(1,n+1):
-        if (n%i) == 0:
-            fn.append(i)
-    
+def gcd(n,m):
+    print(n,m)
     cf = []
-    for i in fm:
-        if i in fn:
+    for i in range(1,min(n,m)+1):
+        if ((m%i) == 0) and ((n%i) == 0):
             cf.append(i)
     
-    return(cf[-1])
+    print(cf[-1])
 
-print(gcd(16,8))
+print("This program will print GCD of two numbers")
+gcd(16,8)
