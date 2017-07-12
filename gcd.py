7b021344a38d32ad8d3f16a58965b166b85e56a5
Sunilkumar <sunillangtad54@gmail.com>
2017-07-12 10:49:40 +0530

    Real Euclid's Algorithm

--- gcd.py
+++ gcd.py
@@ -2,14 +2,10 @@ def gcd(n,m):
     print(n,m)
     if m < n:
         (m,n)=(n,m)
-
-    if ((m%n)==0):
+    if (m%n) == 0:
         return n
     else:
-        diff = m - n
-    
-    # diff > n? Possible!
-    return ( gcd( max(n,diff),min(n,diff) ) )
+        return (gcd(n,(m%n)))
 
 print("This program will print GCD of two numbers")
-print(gcd(16,8))
+print(gcd(16,4))
