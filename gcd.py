c642b3cda61f63c21d423825b3b98785fdeea6d8
Sunilkumar <sunillangtad54@gmail.com>
2017-07-12 09:54:10 +0530

    Running from backwords the minimum number from m and n

--- gcd.py
+++ gcd.py
@@ -1,10 +1,12 @@
 def gcd(n,m):
     print(n,m)
-    for i in range(1,min(n,m)+1):
-        if ((m%i) == 0) and ((n%i) == 0):
-            mrcf=i
-    
-    print(mrcf)
+    i = min(n,m)
+
+    while i > 0:
+        if ((n%i)==0) and ((m%i)==0):
+            print(i)
+            break
+        i=i-1
 
 print("This program will print GCD of two numbers")
-gcd(16,4)
+gcd(16,8)
