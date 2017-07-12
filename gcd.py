0ef8b79f537132cf34f3881458c0b1bfe2869802
Sunilkumar <sunillangtad54@gmail.com>
2017-07-12 09:49:07 +0530

    using most recent common factor

--- gcd.py
+++ gcd.py
@@ -1,11 +1,10 @@
 def gcd(n,m):
     print(n,m)
-    cf = []
     for i in range(1,min(n,m)+1):
         if ((m%i) == 0) and ((n%i) == 0):
-            cf.append(i)
+            mrcf=i
     
-    print(cf[-1])
+    print(mrcf)
 
 print("This program will print GCD of two numbers")
-gcd(16,8)
+gcd(16,4)
